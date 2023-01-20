84ac97098c95bc7d7d2e9af5257e181ba118fb35
Aliaksandr Shavialianchyk <aliaksandr_shavialianchyk@epam.com>
2023-01-20 15:08:03 +0300

    added UTC TimeZone by default

--- web/src/main/java/com/shevelyanchik/fitnessclub/web/WebApplication.java
+++ web/src/main/java/com/shevelyanchik/fitnessclub/web/WebApplication.java
@@ -5,12 +5,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+import java.time.ZoneOffset;
+import java.util.TimeZone;
+
 @EntityScan(basePackages = "com.shevelyanchik.fitnessclub")
 @EnableJpaRepositories(basePackages = "com.shevelyanchik.fitnessclub")
 @SpringBootApplication(scanBasePackages = "com.shevelyanchik.fitnessclub")
 public class WebApplication {
 
     public static void main(String[] args) {
+        TimeZone.setDefault(TimeZone.getTimeZone(ZoneOffset.UTC));
         SpringApplication.run(WebApplication.class, args);
     }
 
