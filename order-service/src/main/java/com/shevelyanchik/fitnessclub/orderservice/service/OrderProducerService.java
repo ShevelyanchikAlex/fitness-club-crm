12d7c31ad00821086d98f42d9e9468f58daf5096
Aliaksandr Shavialianchyk <alexander.shevelianchik@innowise-group.com>
2023-03-01 19:16:55 +0300

    added javadoc to OrderProducerService

--- order-service/src/main/java/com/shevelyanchik/fitnessclub/orderservice/service/OrderProducerService.java
+++ order-service/src/main/java/com/shevelyanchik/fitnessclub/orderservice/service/OrderProducerService.java
@@ -1,7 +1,18 @@
 package com.shevelyanchik.fitnessclub.orderservice.service;
 
-import com.shevelyanchik.fitnessclub.orderservice.model.dto.EmailEvent;
 
+import com.shevelyanchik.fitnessclub.kafkaconfig.dto.EmailEvent;
+
+/**
+ * The OrderProducerService provides the ability to send Message to Topic.
+ *
+ * @version 1.0.1
+ */
 public interface OrderProducerService {
+    /**
+     * Sends Message to Topic.
+     *
+     * @param event Email event
+     */
     void sendMessage(EmailEvent event);
 }
