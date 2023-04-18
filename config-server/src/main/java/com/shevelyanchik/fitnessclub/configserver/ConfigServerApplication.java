6aded0ed10789bba68be45af19067a7bd1cb5141
Aliaksandr Shavialianchyk <alexander.shevelianchik@innowise-group.com>
2023-04-18 13:57:05 +0300

    Refactored global annotations in services

--- config-server/src/main/java/com/shevelyanchik/fitnessclub/configserver/ConfigServerApplication.java
+++ config-server/src/main/java/com/shevelyanchik/fitnessclub/configserver/ConfigServerApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
-@SpringBootApplication
 @EnableConfigServer
+@SpringBootApplication
 public class ConfigServerApplication {
 
 	public static void main(String[] args) {
--- user-service/src/main/java/com/shevelyanchik/fitnessclub/userservice/UserServiceApplication.java
+++ user-service/src/main/java/com/shevelyanchik/fitnessclub/userservice/UserServiceApplication.java
@@ -2,14 +2,10 @@ package com.shevelyanchik.fitnessclub.userservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@EntityScan(basePackages = "com.shevelyanchik.fitnessclub.userservice")
-@EnableJpaRepositories(basePackages = "com.shevelyanchik.fitnessclub.userservice")
-@SpringBootApplication(scanBasePackages = "com.shevelyanchik.fitnessclub.userservice")
 @EnableDiscoveryClient
+@SpringBootApplication
 public class UserServiceApplication {
 
     public static void main(String[] args) {
