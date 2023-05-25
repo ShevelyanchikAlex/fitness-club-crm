220b97142eae6cb67e05eaeffc77db8bab22d912
Aliaksandr Shavialianchyk <alexander.shevelianchik@innowise-group.com>
2023-05-25 13:27:59 +0300

    Refactored User e2e tests

--- user-service/src/test/java/com/shevelyanchik/fitnessclub/userservice/e2e/UserE2ETest.java
+++ user-service/src/test/java/com/shevelyanchik/fitnessclub/userservice/e2e/UserE2ETest.java
@@ -3,8 +3,6 @@ package com.shevelyanchik.fitnessclub.userservice.e2e;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.shevelyanchik.fitnessclub.userservice.constant.Role;
 import com.shevelyanchik.fitnessclub.userservice.constant.Status;
-import com.shevelyanchik.fitnessclub.userservice.e2e.dto.AuthenticationRequest;
-import com.shevelyanchik.fitnessclub.userservice.e2e.dto.AuthenticationResponse;
 import com.shevelyanchik.fitnessclub.userservice.model.dto.UserDto;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
@@ -26,11 +24,8 @@ class UserE2ETest {
     private static final UserDto EXPECTED_USER_DTO = new UserDto(
             1L, "Max", "Filin", "userUseruser01",
             "filin@gmail.com", "+375443321233", "", Role.USER, Status.ACTIVE);
-    private static final AuthenticationRequest EXPECTED_AUTH_REQUEST = new AuthenticationRequest(
-            "filin@gmail.com", "userUseruser01");
     private static final String BASE_API_URL = "http://localhost:8765/api/v1";
     private static final String USER_API_ENDPOINT = "/user-service/users";
-    private static final String AUTH_API_ENDPOINT = "/auth-service/auth";
 
     private TestRestTemplate testRestTemplate;
     private final ObjectMapper objectMapper = new ObjectMapper();
@@ -49,8 +44,8 @@ class UserE2ETest {
     @SneakyThrows
     void testCreateUser() {
         //when
-        String token = login();
-        HttpHeaders headers = configAuthHeader(token);
+        String token = AuthUtils.login(testRestTemplate);
+        HttpHeaders headers = AuthUtils.configAuthHeader(token);
         HttpEntity<UserDto> request = new HttpEntity<>(EXPECTED_USER_DTO, headers);
 
         ResponseEntity<String> response = testRestTemplate.postForEntity(
@@ -72,8 +67,8 @@ class UserE2ETest {
         //given
         long expectedId = EXPECTED_USER_DTO.getId();
         //when
-        String token = login();
-        HttpHeaders headers = configAuthHeader(token);
+        String token = AuthUtils.login(testRestTemplate);
+        HttpHeaders headers = AuthUtils.configAuthHeader(token);
         HttpEntity<Long> request = new HttpEntity<>(expectedId, headers);
 
         ResponseEntity<String> response = testRestTemplate.getForEntity(
@@ -95,8 +90,8 @@ class UserE2ETest {
         //given
         String expectedEmail = EXPECTED_USER_DTO.getEmail();
         //when
-        String token = login();
-        HttpHeaders headers = configAuthHeader(token);
+        String token = AuthUtils.login(testRestTemplate);
+        HttpHeaders headers = AuthUtils.configAuthHeader(token);
         HttpEntity<String> request = new HttpEntity<>(expectedEmail, headers);
 
         ResponseEntity<String> response = testRestTemplate.getForEntity(
@@ -115,28 +110,14 @@ class UserE2ETest {
     @Order(4)
     void testCountUsers() {
         //when
-        String token = login();
-        HttpHeaders headers = configAuthHeader(token);
+        String token = AuthUtils.login(testRestTemplate);
+        HttpHeaders headers = AuthUtils.configAuthHeader(token);
         HttpEntity<String> request = new HttpEntity<>(headers);
-
         ResponseEntity<String> response = testRestTemplate.getForEntity(
                 USER_API_ENDPOINT + "/count", String.class, request);
         //then
         Assertions.assertEquals(HttpStatus.OK, response.getStatusCode());
         Assertions.assertNotNull(response.getBody());
-    }
-
-
-    private String login() {
-        ResponseEntity<AuthenticationResponse> response = testRestTemplate.postForEntity(
-                AUTH_API_ENDPOINT + "/login", EXPECTED_AUTH_REQUEST, AuthenticationResponse.class);
-        return response.getBody().getToken();
-    }
-
-    private HttpHeaders configAuthHeader(String token) {
-        HttpHeaders headers = new HttpHeaders();
-        headers.add("Authorization", token);
-        return headers;
     }
 
 }
