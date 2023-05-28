23441d5894306184f4151d0bd641baecaf3011b1
Aliaksandr Shavialianchyk <alexander.shevelianchik@innowise-group.com>
2023-05-28 19:53:46 +0300

    Refactored Role enum

--- user-service/src/main/java/com/shevelyanchik/fitnessclub/userservice/constant/Role.java
+++ user-service/src/main/java/com/shevelyanchik/fitnessclub/userservice/constant/Role.java
@@ -2,11 +2,13 @@ package com.shevelyanchik.fitnessclub.userservice.constant;
 
 import com.shevelyanchik.fitnessclub.userservice.exception.ValidationException;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
 import java.util.Arrays;
 import java.util.Set;
 
 @Getter
+@RequiredArgsConstructor
 public enum Role {
     GUEST(Set.of(Permission.GUEST_PERMISSION)),
     USER(Set.of(Permission.USER_PERMISSION)),
@@ -14,10 +16,6 @@ public enum Role {
     ADMIN(Set.of(Permission.USER_PERMISSION, Permission.TRAINER_PERMISSION, Permission.ADMIN_PERMISSION));
 
     private final Set<Permission> permissions;
-
-    Role(Set<Permission> permissions) {
-        this.permissions = permissions;
-    }
 
     public static Role getRoleByName(String roleName) {
         return Arrays.stream(Role.values())
